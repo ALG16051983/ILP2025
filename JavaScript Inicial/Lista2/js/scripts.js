bc378210dc53ed9197ae5db302cc69377a6988b9
ANDRE LUIS GIMENEZ <1090482421024@fatec-srv.local>
2025-03-17 11:05:16 -0300

    Correcao Lista 2 ate 5

--- JavaScript Inicial/Lista2/js/scripts.js
+++ JavaScript Inicial/Lista2/js/scripts.js
@@ -3,25 +3,33 @@ function seguir() {
   }
 function exe1() {
     // Obter os valores das notas dos campos de entrada
-    const nota1 = parseFloat(document.getElementById("nota1").value);
-    const nota2 = parseFloat(document.getElementById("nota2").value);
-    const nota3 = parseFloat(document.getElementById("nota3").value);
-    const nota4 = parseFloat(document.getElementById("nota4").value);
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
+    let nota3 = Number(document.getElementById("nota3").value);
+    let nota4 = Number(document.getElementById("nota4").value);
   
     // Calcular a média
-    const media = (nota1 + nota2 + nota3 + nota4) / 4;
+    let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    //Definir status media - APROVADO OU REPROVADO
+    let status="";
+    if(media < 7){
+      status="Reprovado";
+    } else {
+      status ="Aprovado";
+    }
   
     // Exibir a média no elemento label com o id "media"
-    document.getElementById("sub").textContent = "A média do aluno é: " + media.toFixed(2); //toFixed(2) para duas casas decimais
+    document.getElementById("media").textContent = "A média do aluno é: " + media + " - Status: " + status
   }
   function exe2() {
     // Obter os valores das notas dos campos de entrada
-    const nota1 = parseFloat(document.getElementById("nota1").value);
-    const nota2 = parseFloat(document.getElementById("nota2").value);
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
     
   
     // Calcular a média
-    const media = (nota1 + nota2) / 2;
+    let media = (nota1 + nota2) / 2;
 
     // Determinar o status da media
     let status="";
@@ -38,8 +46,8 @@ function exe1() {
   }
   function exe3() {
     // Obter os valores dos numeros dos campos de entrada
-    const numero1 = parseFloat(document.getElementById("numero1").value);
-    const numero2 = parseFloat(document.getElementById("numero2").value);
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
       
     // Determinar o menor
     let menor;
@@ -53,9 +61,9 @@ function exe1() {
   }
   function exe4() {
     // Obter os valores dos numeros dos campos de entrada
-    const numero1 = parseFloat(document.getElementById("numero1").value);
-    const numero2 = parseFloat(document.getElementById("numero2").value);
-    const numero3 = parseFloat(document.getElementById("numero3").value);
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+    let numero3 = Number(document.getElementById("numero3").value);
       
     // Determinar o maior
     let maior;
@@ -72,9 +80,9 @@ function exe1() {
   }
   function exe5() {
     // Obter os valores dos números dos campos de entrada
-    const numero1 = parseFloat(document.getElementById("numero1").value);
-    const numero2 = parseFloat(document.getElementById("numero2").value);
-    const operacao = parseInt(document.getElementById("operacao").value);
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+    let operacao = parseInt(document.getElementById("operacao").value);
   
     // Validar os números
     if (isNaN(numero1) || isNaN(numero2)) {
